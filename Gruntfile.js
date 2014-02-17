1a36ddf8c0390d3d6fcfe75569106c0cc6183b02
vagrant <vagrant@precise32.(none)>
2014-02-17 07:12:52 +0000

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    // 加载包含 "jshint" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['jshint']);
+
+};
