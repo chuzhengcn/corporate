b07d7b516c76e487c23dc475c62361bb6a3e8ff6
chuzheng <chuzheng@gmail.com>
2014-04-08 18:57:50 +0800

    xiaoxiong farmer

--- routes.js
+++ routes.js
@@ -1,11 +1,15 @@
-var index = require('./controller/index'),
-    weixin = require('./controller/weixin');
+var index           = require('./controller/index'),
+    weixin          = require('./controller/weixin'),
+    xiaoxiong_index = require('./controller/xiaoxiong/index');
 
 function init_routes(app) {
     app.get('/', index.index);
     app.get('/create_weixin_menu', weixin.create_menu)
     app.get('/weixin', weixin.verify)
     app.post('/weixin', weixin.msg)
+
+    // xiaoxiong
+    app.get('/xiaoxiong', xiaoxiong_index.index)
 }
 
 exports = module.exports = init_routes;
