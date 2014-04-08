1b9d0c46844986edd48f57c381fe1c31f97f1862
chuzheng <chuzheng@gmail.com>
2014-04-08 11:11:04 +0800

    weixin verify

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
-var index = require('./controller/index')
+var index = require('./controller/index'),
+    weixin = require('./controller/weixin');
 
 function init_routes(app) {
     app.get('/', index.index);
+    app.get('/weixin/verify', weixin.verify)
 }
 
 exports = module.exports = init_routes;
---
+++ routes/weixin.js
@@ -0,0 +1,27 @@
+var TOKEN   = process.env.WEIXIN_TOKEN || 'feiyesoft1984',
+    crypto  = require('crypto'),
+    util    = require('util'),
+    async   = require('async'),
+    moment  = require('moment');
+
+// -------------------------------------------------------------------------------------------------------------
+function verify(req, res) {
+    var shasum              = crypto.createHash('sha1'),
+        signature           = req.query.signature,
+        echostr             = req.query.echostr,
+        unencrypted_params  = [TOKEN, req.query.timestamp, req.query.nonce].sort().join().replace(/,/g, ''),
+        encrypted_str       = '';
+
+    shasum.update(unencrypted_params) 
+
+    encrypted_str = shasum.digest('hex')
+
+    if (encrypted_str === signature) {
+        res.send(echostr)
+    } else {
+        res.send({ok : 0, msg : '验证失败'})        
+    }
+}
+
+exports.verify = verify
+// ----------------------------------------------------------------------------------------------------------------------
