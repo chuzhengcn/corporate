6744887d7edc75a5460f89dd5d77b9390b3ec986
chuzheng <chuzheng@gmail.com>
2014-04-22 11:41:13 +0800

    upload pic

--- xiaoxiong/admin/c_upload.js
+++ xiaoxiong/admin/c_upload.js
@@ -1,3 +1,20 @@
+var fs              = require('fs');
+
 exports.index = function(req, res) {
-    console.log(req.files)
+    var input_file_name = "thumbnail",
+        max_size        = 5000000,
+        local_path      = __dirname + '/../../public/xiaoxiong/_att',
+        upload_file     = req.files[input_file_name],
+        http_url        = "http://www.xiaoxiong.com/_att";
+
+    if (upload_file.size > max_size) {
+        fs.unlink(upload_file.path)
+        return res.send({ok : 0})
+    }
+
+    var changed_file_name = upload_file.path.slice(upload_file.path.lastIndexOf('/')).toLocaleLowerCase();
+
+    fs.rename(upload_file.path, local_path + changed_file_name, function() {
+        res.send({ ok : 1, url : http_url + changed_file_name })
+    })
 }
