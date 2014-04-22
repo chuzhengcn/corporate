87cb3b2114f90b50d80c103b92382d69ea86c551
chuzheng <chuzheng@gmail.com>
2014-04-22 12:23:37 +0800

    add login

--- xiaoxiong/admin/c_recipe.js
+++ xiaoxiong/admin/c_recipe.js
@@ -1,32 +1,39 @@
 var util        = require("util"),
     lib_util    = require("../../lib/util"),
-    m_recipe      = require("../m_recipe").Recipe;
+    login       = require('./c_login'),
+    m_recipe    = require("../m_recipe").Recipe;
 
 
 exports.index = function (req, res) {
-    var page = parseInt(req.query.page) || 1;
-    m_recipe.find_by_page(page, function(err, docs) {
-        if (err) {
-            console.log(err)
-        }
+    login.check_login_and_send(req, res, function() {
+        var page = parseInt(req.query.page) || 1;
+        m_recipe.find_by_page(page, function(err, docs) {
+            if (err) {
+                console.log(err)
+            }
 
-        res.render('xiaoxiong/admin/recipe', {recipe_list : docs})  
+            res.render('xiaoxiong/admin/recipe', {recipe_list : docs})  
+        })
     })
 }
 
 exports.create_page = function(req, res) {
-    res.render('xiaoxiong/admin/create_recipe')
+    login.check_login_and_send(req, res, function() {
+        res.render('xiaoxiong/admin/create_recipe')
+    })
 }
 
 exports.create = function(req, res) {
-    var doc = {
-        title : req.body.title,
-        content : req.body.content,
-        thumbnail: req.body.thumbnail,
-        original_price: req.body.original_price,
-        price: req.body.price,
-        create_at : Date.now()
-    }
+    login.check_login_and_send(req, res, function() {
+        var doc = {
+            title : req.body.title,
+            content : req.body.content,
+            thumbnail: req.body.thumbnail,
+            original_price: req.body.original_price,
+            price: req.body.price,
+            create_at : Date.now()
+        }
 
-    m_recipe.create()
+        m_recipe.create()
+    })
 }
--- xiaoxiong/admin/c_upload.js
+++ xiaoxiong/admin/c_upload.js
@@ -1,20 +1,23 @@
-var fs              = require('fs');
+var fs              = require('fs'),
+    login           = require('./c_login');
 
 exports.index = function(req, res) {
-    var input_file_name = "thumbnail",
-        max_size        = 5000000,
-        local_path      = __dirname + '/../../public/xiaoxiong/_att',
-        upload_file     = req.files[input_file_name],
-        http_url        = "http://www.feiyesoft.com/xiaoxiong/_att";
+    login.check_login_and_send(req, res, function() {
+        var input_file_name = "thumbnail",
+            max_size        = 5000000,
+            local_path      = __dirname + '/../../public/xiaoxiong/_att',
+            upload_file     = req.files[input_file_name],
+            http_url        = "http://www.feiyesoft.com/xiaoxiong/_att";
 
-    if (upload_file.size > max_size) {
-        fs.unlink(upload_file.path)
-        return res.send({ok : 0})
-    }
+        if (upload_file.size > max_size) {
+            fs.unlink(upload_file.path)
+            return res.send({ok : 0})
+        }
 
-    var changed_file_name = upload_file.path.slice(upload_file.path.lastIndexOf('/')).toLocaleLowerCase();
+        var changed_file_name = upload_file.path.slice(upload_file.path.lastIndexOf('/')).toLocaleLowerCase();
 
-    fs.rename(upload_file.path, local_path + changed_file_name, function() {
-        res.send({ ok : 1, url : http_url + changed_file_name })
+        fs.rename(upload_file.path, local_path + changed_file_name, function() {
+            res.send({ ok : 1, url : http_url + changed_file_name })
+        })
     })
 }
