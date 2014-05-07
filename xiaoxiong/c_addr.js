60178e66c163d2bae3f9faf9d291cab3473624b3
chuzheng <chuzheng@gmail.com>
2014-05-07 14:04:35 +0800

    addr

--- xiaoxiong/c_addr.js
+++ xiaoxiong/c_addr.js
@@ -32,9 +32,9 @@ function send_event_my_addr_response(req, res) {
         }
 
         if (!doc) {
-            content = '您还没有收货地址。<a href="' + create_addr_url + '">现在添加</a>';
+            content = '您还没有收货地址。\t\r <a href="' + create_addr_url + '">现在添加</a>';
         } else {
-            content = "您的常用地址：" + doc.name + " " + doc.tel + " " + doc.area + " " + doc.detail +
+            content = "您的常用地址：\t\r " + doc.userName + " " + doc.telNumber + " " + doc.area + " " + doc.detail +
                       "。<a href='" + all_addr_page + "'>修改</a>"; 
         }
 
